9a61102972e683b2fcaacc01b0be3d54ad7ee27d
adlye1997 <549111916@qq.com>
2024-02-20 23:28:39 +0800

    update stm32f103_bsp_flash

--- stm32f103_bsp_flash.c
+++ stm32f103_bsp_flash.c
@@ -1,70 +1,63 @@
 #include "stm32f103_bsp_flash.h"
-#include "bsp_usart.h"
 
-HAL_StatusTypeDef WriteFlashU32(uint32_t *data, uint16_t data_len, uint32_t address)
-{
-	HAL_StatusTypeDef status = HAL_FLASH_Unlock();
-	if (status == HAL_OK) {
+HAL_StatusTypeDef WriteFlashU32(uint32_t *data, uint32_t data_len, uint32_t address) {
+	HAL_StatusTypeDef res = HAL_FLASH_Unlock();
+	if (res == HAL_OK) {
 			for (uint32_t i = 0; i < data_len; i++) {
-					status = HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, address, data[i]);
-					if (status != HAL_OK) {
-						return status;
+					res = HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, address, data[i]);
+					if (res != HAL_OK) {
+						return res;
 					}
 					address += 4;
 			}
 			HAL_FLASH_Lock();
 	}
-	return status;
+	return res;
 }
 
-HAL_StatusTypeDef OverWriteFlashU32(uint32_t *data, uint16_t data_len, uint32_t address)
-{
-	EraseFlash(address, data_len * 4);
-	HAL_StatusTypeDef status = HAL_FLASH_Unlock();
-	if (status == HAL_OK) {
+HAL_StatusTypeDef OverWriteFlashU32(uint32_t *data, uint32_t data_len, uint32_t address) {
+	HAL_StatusTypeDef res = EraseFlash(address, data_len * 4);
+	res |= HAL_FLASH_Unlock();
+	if (res == HAL_OK) {
 			for (uint32_t i = 0; i < data_len; i++) {
-					status = HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, address, data[i]);
-					if (status != HAL_OK) {
-						return status;
+					res = HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, address, data[i]);
+					if (res != HAL_OK) {
+						return res;
 					}
 					address += 4;
 			}
 			HAL_FLASH_Lock();
 	}
-	return status;
+	return res;
 }
 
-void ReadFlashU32(uint32_t *data, uint16_t data_len, uint32_t address)
-{
-	for(uint16_t i = 0; i < data_len; i++)
-	{
+void ReadFlashU32(uint32_t *data, uint32_t data_len, uint32_t address) {
+	for(uint32_t i = 0; i < data_len; i++) 	{
 		*data = *(uint32_t*)address;
 		data += 1;
 		address += 4;
 	}
 }
 
-HAL_StatusTypeDef EraseFlashPage(uint32_t address, uint16_t page_num)
-{
-	HAL_StatusTypeDef status = HAL_FLASH_Unlock();
-
-	if (status == HAL_OK) {
+HAL_StatusTypeDef EraseFlashPage(uint32_t address, uint16_t page_num) {
+	HAL_StatusTypeDef res = HAL_FLASH_Unlock();
+	if (res == HAL_OK) {
 		uint32_t PageError = 0;
 		FLASH_EraseInitTypeDef EraseInitStruct;
-
 		EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
 		EraseInitStruct.PageAddress = address;
 		EraseInitStruct.NbPages = page_num;
-		status = HAL_FLASHEx_Erase(&EraseInitStruct, &PageError);
-
-    HAL_FLASH_Lock();
+		res |= HAL_FLASHEx_Erase(&EraseInitStruct, &PageError);
+    	HAL_FLASH_Lock();
 	}
-	return status;
+	return res;
 }
 
 uint32_t flash_reserve1[256];
 uint32_t flash_reserve2[256];
-void EraseFlash(uint32_t address, uint16_t length) {
+HAL_StatusTypeDef EraseFlash(uint32_t address, uint32_t length) {
+	HAL_StatusTypeDef res = HAL_OK;
+
 	uint32_t address_erase_begin = (address - FLASH_SECTOR_BASE_ADDRESS) / FLASH_PAGE_SIZE * FLASH_PAGE_SIZE + FLASH_SECTOR_BASE_ADDRESS;
 	uint32_t address_erase_end = (address + length - FLASH_SECTOR_BASE_ADDRESS) / FLASH_PAGE_SIZE * FLASH_PAGE_SIZE + FLASH_SECTOR_BASE_ADDRESS + FLASH_PAGE_SIZE - 1;
 	uint32_t erase_page_num = (address_erase_end - address_erase_begin) / FLASH_PAGE_SIZE + 1;
@@ -75,7 +68,9 @@ void EraseFlash(uint32_t address, uint16_t length) {
 
 	ReadFlashU32(flash_reserve1, reserve1_length / 4, address_erase_begin);
 	ReadFlashU32(flash_reserve2, reserve2_length / 4, address_reserve2_begin);
-	EraseFlashPage(address_erase_begin, erase_page_num);
-	WriteFlashU32(flash_reserve1, reserve1_length / 4, address_erase_begin);
-	WriteFlashU32(flash_reserve2, reserve2_length / 4, address_reserve2_begin);
+	res |= EraseFlashPage(address_erase_begin, erase_page_num);
+	res |= WriteFlashU32(flash_reserve1, reserve1_length / 4, address_erase_begin);
+	res |= WriteFlashU32(flash_reserve2, reserve2_length / 4, address_reserve2_begin);
+
+	return res;
 }
--- stm32f103_bsp_flash.h
+++ stm32f103_bsp_flash.h
@@ -6,10 +6,10 @@
 #define FLASH_SECTOR_BASE_ADDRESS 0x08000000
 // #define FLASH_PAGE_SIZE 0x400
 
-HAL_StatusTypeDef OverWriteFlashU32(uint32_t *data, uint16_t data_len, uint32_t address);
-HAL_StatusTypeDef WriteFlashU32(uint32_t *data, uint16_t data_len, uint32_t address);
-void ReadFlashU32(uint32_t *data, uint16_t data_len, uint32_t address);
+HAL_StatusTypeDef OverWriteFlashU32(uint32_t *data, uint32_t data_len, uint32_t address);
+HAL_StatusTypeDef WriteFlashU32(uint32_t *data, uint32_t data_len, uint32_t address);
+void ReadFlashU32(uint32_t *data, uint32_t data_len, uint32_t address);
 HAL_StatusTypeDef EraseFlashPage(uint32_t address, uint16_t page_num);
-void EraseFlash(uint32_t address, uint16_t length);
+HAL_StatusTypeDef EraseFlash(uint32_t address, uint32_t length);
 
 #endif
